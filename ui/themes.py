76d2195c335696e0d2f3e877b135d5a64c6f7132
T1rin <t1rin@mail.ru>
2026-06-03 12:37:37 +0300

    update themes.py

--- ui/themes.py
+++ ui/themes.py
@@ -1,19 +1,12 @@
 import dearpygui.dearpygui as dpg
 
 global_font_path = "./assets/fonts/DeleddaOpen.ttf"
-menu_font_path = "./assets/fonts/DeleddaOpen.ttf"
 about_font_path = "./assets/fonts/DeleddaOpen.ttf"
 
 def register_themes():
     with dpg.font_registry():
-        with dpg.font(global_font_path, size=14, tag="global_font"):
-            dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
-
-        with dpg.font(menu_font_path, size=12, tag="menu_font"):
-            dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
-
-        with dpg.font(about_font_path, size=16, tag="about_font"):
-            dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
+        with dpg.font(global_font_path, size=14, tag="global_font"): pass
+        with dpg.font(about_font_path, size=16, tag="about_font"): pass
 
     with dpg.theme(tag="global_theme"):
         with dpg.theme_component(dpg.mvAll):
